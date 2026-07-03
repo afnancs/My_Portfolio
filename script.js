b4a6d5d05070bada7e7249bec57182895d665a0e
afnancs <afnanahmedcs@gmail.com>
2026-07-03 12:12:42 -0700

    Final Portfolio Updata

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById("viewprojects").onclick = function () { document .getElementById("projects") .scrollIntoView({
+        behavior:"smooth"
+    });
+};
+
+
+document.getElementById("topBtn").onclick=function(){
+
+window.scrollTo({
+
+top:0,
+
+behavior:"smooth"
+
+});
+
+}
